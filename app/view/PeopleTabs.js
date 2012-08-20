4042e40df094f5a846292722438d83dc0b80d678
Joshua Dover <gerfuls@gmail.com>
2012-08-20 15:42:13 -0500

    Created tab bar view for people tab

---
+++ app/view/PeopleTabs.js
@@ -0,0 +1,30 @@
+Ext.define('Bongo.view.PeopleTabs', {
+	extend: 'Ext.tab.Panel',
+	
+	config: {
+		ui: 'light',
+		tabBar: {
+			layout: {
+				pack: 'center'
+			}
+		},
+		activeTab: 1,
+		defaults: {
+			scrollable: true
+		},
+		items: [
+			{
+				title: 'Updates',
+				cls: 'card'
+			},
+			{
+				title: 'My Friends',
+				cls: 'card'
+			},
+			{
+				title: 'Clubs',
+				cls: 'card'
+			}
+		]
+	}
+});
