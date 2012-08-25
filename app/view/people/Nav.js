ea90128b632dfcd335da06de039fbe79fd8cbc7b
Joshua Dover <gerfuls@gmail.com>
2012-08-25 11:04:52 -0500

    Remove listeners, need to be moved to controller 'Main'

--- app/view/people/Nav.js
+++ app/view/people/Nav.js
@@ -18,43 +18,6 @@ Ext.define('Bongo.view.people.Nav', {
 					iconCls: 'settings',
 					iconMask: true,
 					align: 'left',
-					listeners: {
-						tap: function() {
-							if (!this.overlay) {
-								this.overlay = Ext.Viewport.add({
-									xtype: 'panel',
-									modal: true,
-									hideOnMaskTap: true,
-									showAnimation: {
-										type: 'popIn',
-										duration: 250,
-										easing: 'ease-out'
-									},
-									hideAnimation: {
-										type: 'popOut',
-										duration: 250,
-										easing: 'ease-out'
-									},
-									centered: true,
-									width: Ext.os.deviceType == 'Phone' ? 260 : 400,
-									height: Ext.os.deviceType == 'Phone' ? 220 : 400,
-									styleHtmlContent: true,
-									html: '<p>This is a modal, centered and floating panel. hideOnMaskTap is true by default so ' +
-										'we can tap anywhere outside the overlay to hide it.</p>',
-									items: [
-										{
-											docked: 'top',
-											xtype: 'toolbar',
-											title: 'Settings'
-										}
-									],
-									scrollable: true
-								});
-							}
-		
-							this.overlay.show();
-						}
-					}
 				},
 				{
 					xtype: 'button',
